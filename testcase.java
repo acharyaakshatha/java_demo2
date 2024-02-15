ee4302f58ce63e339bb8df55e191dd034fc33748
acharyaakshatha <159416685+acharyaakshatha@users.noreply.github.com>
2024-02-14 22:23:01 -0800

    Add files via upload

---
+++ testcase.java
@@ -0,0 +1,22 @@
+class testcase
+ {
+   public static void main(String args[])
+    {
+      String country="India";
+      switch (country)
+       {
+         case "India":
+         System.out.println("Capital is New delhi");
+         break;
+         case "China":
+         System.out.println("Capital is Bejing");
+         break;
+         case "Japan":
+         System.out.println("Capital is Tokiyo");
+         break;
+         case "default":
+         System.out.println("not found");
+       }
+    }
+ }
+
